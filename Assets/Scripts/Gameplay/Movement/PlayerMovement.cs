588fc5c0e42b7541c6b97f1799db8c3f1e93c8a5
CaineTinyArms <146839070+CaineTinyArms@users.noreply.github.com>
2026-07-25 19:15:25 +0100

    replaced speed with momentum based variables (numbers are gibberish for now)

--- Assets/Scripts/Gameplay/Movement/PlayerMovement.cs
+++ Assets/Scripts/Gameplay/Movement/PlayerMovement.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMove : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     //movement variables
     float x;
@@ -14,12 +14,11 @@ public class PlayerMove : MonoBehaviour
 
     //movement variables
     [Header("Movement Variables")]
-    public float speed = 12.0f;
+    [SerializeField] private float maxSpeed = 8.0f;
+    [SerializeField] private float acceleration = 18.0f;
+    [SerializeField] private float deceleration = 20.0f;
+    private Vector3 currentVelocity = Vector3.zero;
 
-    private void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
